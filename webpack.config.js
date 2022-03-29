63fa879c2be5560253f4a28588aa411af2d15827
Matt Ross <amsross@gmail.com>
2022-03-29 11:16:52 -0400

    chore(dev): use webpack instead of watcher

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,31 @@
 const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
-  entry: './lib/js/src/Index.bs.js',
-  // If you ever want to use webpack during development, change 'production'
-  // to 'development' as per webpack documentation. Again, you don't have to
-  // use webpack or any other bundler during development! Recheck README if
-  // you didn't know this
-  mode: 'production',
+  entry: './lib/es6/src/Index.bs.js',
+  mode: isProd ? "production" : "development",
+  externals: {
+    'react-dom': 'ReactDOM',
+    'react': 'React',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "static/index.html",
+      inject: false,
+    }),
+  ],
+  devServer: {
+    compress: true,
+    port: process.env.PORT || 8000,
+    historyApiFallback: true,
+  },
+  externals: {
+    'chart.js': 'Chart',
+    'react': 'React',
+    'react-dom': 'ReactDOM',
+  },
   output: {
     path: path.join(__dirname, "dist"),
     filename: 'index.js',
